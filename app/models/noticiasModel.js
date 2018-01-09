701b97eeb97e7d255e57e2c4abbac3d30bb0ad08
rogeriolucio <rogerioluciodasilva@hotmail.com>
2018-01-09 17:37:59 -0200

    atualizacao - implementacao do insert

--- app/models/noticiasModel.js
+++ app/models/noticiasModel.js
@@ -9,5 +9,11 @@ module.exports = function(){
 	this.getNoticia = function(connection,callback){
 		connection.query('SELECT * FROM noticias where id_noticia = 2', callback);
 	}
+
+	this.salvarNoticia = function(noticia,connection,callback){
+		connection.query("INSERT INTO noticias SET ?",noticia, callback);
+	}
 	return this;
+
+
 }
--- app/routes/admin.js
+++ app/routes/admin.js
@@ -5,7 +5,18 @@ module.exports = function(app){
 	});
 
 	app.post('/noticias/salvar', function(req,res){
-		var noticias = req.body;
-		res.send(noticias);
+		var noticia = req.body;
+
+		var connection = app.config.dbConnection();
+		var noticiasModel = app.app.models.noticiasModel;
+
+		noticiasModel.salvarNoticia(noticia, connection, function(error, result){
+			//res.render("noticias/noticias",{noticias:result});
+			res.redirect('/noticias');
+		});
+		
 	});
+
+
+	
 };
